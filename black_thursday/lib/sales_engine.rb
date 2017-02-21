7371a2d9c6f1a0fbc79d27b6d61e43bf0f3d6ee9
NatBee <nataliesbarron@gmail.com>
2017-02-21 14:08:39 -0700

    started tdd and pseudocode sales_engine

---
+++ black_thursday/lib/sales_engine.rb
@@ -0,0 +1,1 @@
+# create search_engine class
---
+++ black_thursday/test/sales_engine_test.rb
@@ -0,0 +1,21 @@
+require './lib/search_engine'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class SalesEngineTest < Minitest::Test
+
+  def test_it_exists
+    se = SalesEngine.new
+    assert_instance_of SalesEngine, se
+  end
+
+  def test_from_csv_method
+    se = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+    
+  end
+
+end
