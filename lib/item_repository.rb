6361db3a271668c322cc44385bdf4b3cc2b949b6
NatBee <nataliesbarron@gmail.com>
2017-02-23 12:01:59 -0700

    finished iteration 0 v1

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,6 +1,47 @@
 require 'pry'
+require 'bigdecimal'
 
 class ItemRepository
-  def initialize(hash, parent)
+
+  def initialize(raw_item_data, parent)
+    @item_list = raw_item_data.map { |line| Item.new(line, self) }
+    @parent = parent
   end
+
+  def all
+      @item_list
+  end
+
+  def find_by_id(id)
+    @item_list.find { |item| item.id == id }
+  end
+
+  def find_by_name(name)
+    @item_list.find { |item| item.name.downcase == name.downcase }
+  end
+
+  def find_all_by_description(description_snippet)
+    @item_list.find_all do |item|
+      item.description.downcase.include?(description_snippet.downcase)
+    end
+  end
+
+  def find_all_by_price(price)
+    @item_list.find_all do |item|
+     item.unit_price == BigDecimal.new(price)
+   end
+  end
+
+  def find_all_by_price_in_range(range)
+    @item_list.find_all do |item|
+      (range).member?(item.unit_price)
+    end
+  end
+
+  def find_all_by_merchant_id(merchant_id_lookup)
+    @item_list.find_all do |item|
+      item.merchant_id == merchant_id_lookup
+    end
+  end
+  
 end
